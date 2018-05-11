6190ee0ce766f9eba547f16af8422539b3d03756
Juho Lehtinen <lehtu05@gmail.com>
2018-05-12 00:21:08 +0300

    Maybe last commits about arto?

--- osa1/tehtava-1.0/src/index.js
+++ osa1/tehtava-1.0/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
+class Hello extends React.Component {
+  render() {
+    const {name, age} = this.props
+    const bornYear = () => 1900 + new Date().getYear() - age
+
+    return (
+      <div>
+        <p>
+          Hello {name}, you are {age} years old <br />
+          So you were propably born {bornYear()}
+        </p>
+      </div>
+    )
+  }
 }
 
 const App = () => {
