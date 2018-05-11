336d608fa34cc6041e859ed07724a21a65cdceb3
Juho Lehtinen <lehtu05@gmail.com>
2018-05-12 02:36:21 +0300

    Added solution for 1.10

---
+++ osa1/tehtava-1.10/README.md
@@ -0,0 +1,5 @@
+# osa1 tehtava-1.10
+
+The task: (1.10)[https://fullstackopen.github.io/teht%C3%A4v%C3%A4t/#110-unicafe-osa5]
+
+Time spent: 7 minutes
---
+++ osa1/tehtava-1.10/src/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({onClick, value}) => (
+  <button onClick={onClick}>{value}</button>
+)
+
+const Statistics = ({good, neutral, bad}) => {
+  const votes = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / votes || 0
+  const percentage = good / votes || 0
+
+  if (votes === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        Ei yhtään palautetta annettu
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <Statistic label="good" value={good} />
+      <Statistic label="neutral" value={neutral} />
+      <Statistic label="bad" value={bad} />
+      <Statistic label="avg" value={average.toFixed(1)} />
+      <Statistic label="percentage" value={percentage.toFixed(2) + '%'} />
+    </div>
+  )
+}
+
+const Statistic = ({label, value}) => (
+  <div>
+    <span>{label} {value}</span><br />
+  </div>
+)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    }
+  }
+
+  updateHandler = (data) => () => this.setState(data)
+
+  render() {
+    return (
+      <div>
+        <h1>give us feedback</h1>
+        <Button onClick={this.updateHandler({good: this.state.good + 1})} value="good" />
+        <Button onClick={this.updateHandler({neutral: this.state.neutral + 1})} value="neutral" />
+        <Button onClick={this.updateHandler({bad: this.state.bad + 1})} value="bad" />
+
+        <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
