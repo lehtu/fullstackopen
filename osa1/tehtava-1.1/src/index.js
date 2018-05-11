850369073f02c5598d2cee8935d425fb4c3f0628
Juho Lehtinen <lehtu05@gmail.com>
2018-05-11 22:45:16 +0300

    Fixed translations

--- osa1/tehtava-1.1/src/index.js
+++ osa1/tehtava-1.1/src/index.js
@@ -19,18 +19,18 @@ const Total = (props) => (
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const section1 = 'Reactin perusteet'
+  const tasks1 = 10
+  const section2 = 'Tiedonvälitys propseilla'
+  const tasks2 = 7
+  const section3 = 'Komponenttien tila'
+  const tasks3 = 14
 
   return (
     <div>
       <Title value={course} />
-      <Content part1={osa1 + " " + tehtavia1} part2={osa2 + " " + tehtavia2} part3={osa3 + " " + tehtavia3} />
-      <Total amount={tehtavia1 + tehtavia2 + tehtavia3} />
+      <Content part1={section1 + " " + tasks1} part2={section2 + " " + tasks2} part3={section3 + " " + tasks3} />
+      <Total amount={tasks1 + tasks2 + tasks3} />
     </div>
   )
 }
