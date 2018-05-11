2f12f3c4e4844952390d50622c711bec2a4e8684
Juho Lehtinen <lehtu05@gmail.com>
2018-05-12 00:04:57 +0300

    Added solution for 1.5

---
+++ osa1/tehtava-1.5/README.md
@@ -0,0 +1,5 @@
+# osa1 tehtava-1.5
+
+The task: (1.5)[https://fullstackopen.github.io/teht%C3%A4v%C3%A4t/#15]
+
+Time spent: 3 minutes
---
+++ osa1/tehtava-1.5/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Title = (props) => (
+  <h1>{props.value}</h1>
+)
+
+const Content = (props) => (
+  <div>
+    <Section value={props.sections[0]} />
+    <Section value={props.sections[1]} />
+    <Section value={props.sections[2]} />
+  </div>
+)
+
+const Total = (props) => (
+  <p>yhteensä {props.sections[0].tasks + props.sections[1].tasks + props.sections[2].tasks} tehtävää</p>
+)
+
+const Section = (props) => (
+  <p>{props.value.name} {props.value.tasks}</p>
+)
+
+const App = () => {
+  const course = {
+    title: 'Half Stack -sovelluskehitys',
+    sections: [
+      {
+        name: 'Reactin perusteet',
+        tasks: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        tasks: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        tasks: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Title value={course.title} />
+      <Content sections={course.sections} />
+      <Total sections={course.sections} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
