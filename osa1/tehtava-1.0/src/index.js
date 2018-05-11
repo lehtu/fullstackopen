d8ddf719e07324f715d17650276ea47ff34a5284
Juho Lehtinen <lehtu05@gmail.com>
2018-05-12 01:12:11 +0300

    Finished more complex state handling and array and optional rendering examples

--- osa1/tehtava-1.0/src/index.js
+++ osa1/tehtava-1.0/src/index.js
@@ -1,41 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      counter: 1
+      vasen: 0,
+      oikea: 0,
+      kaikki: []
     }
   }
 
-  asetaArvoon = (arvo) => () => this.setState({ counter: arvo })
+  klikVasen = () => {
+    this.setState({
+      vasen: this.state.vasen + 1,
+      kaikki: this.state.kaikki.concat('v')
+    })
+  }
+
+  klikOikea = () => {
+    this.setState({
+      oikea: this.state.oikea + 1,
+      kaikki: this.state.kaikki.concat('o')
+    })
+  }
 
   render() {
+    const historia = () => {
+      if (this.state.kaikki.length === 0) {
+        return (
+          <div>
+            <em>sovellusta käytetään nappeja painelemalla</em>
+          </div>
+        )
+      }
+      return (
+        <div>
+          näppäilyhistoria: {this.state.kaikki.join(' ')}
+        </div>
+      )
+    }
     return (
       <div>
-        <Display counter={this.state.counter}/>
         <div>
-          <Button
-            handleClick={this.asetaArvoon(this.state.counter + 1)}
-            text="Plus"
-          />
-          <Button
-            handleClick={this.asetaArvoon(this.state.counter - 1)}
-            text="Minus"
-          />
-          <Button
-            handleClick={this.asetaArvoon(0)}
-            text="Zero"
-          />
+          {this.state.vasen}
+          <button onClick={this.klikVasen}>vasen</button>
+          <button onClick={this.klikOikea}>oikea</button>
+          {this.state.oikea}
+          <div>{historia()}</div>
         </div>
       </div>
     )
