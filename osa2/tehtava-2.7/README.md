522c802995d7b0739b1e280229c5a0a0d90ec9de
Juho Lehtinen <lehtu05@gmail.com>
2018-05-14 22:23:44 +0300

    Added solution for 2.7

---
+++ osa2/tehtava-2.7/README.md
@@ -0,0 +1,5 @@
+# osa2 tehtava-2.7
+
+The task: (2.7)[https://fullstackopen.github.io/teht%C3%A4v%C3%A4t/#27-puhelinluettelo-osa-2]
+
+Time spent: 5 minutes
---
+++ osa2/tehtava-2.7/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addName = (event) => {
+    event.preventDefault();
+
+    const nameExists = !!this.state.persons.find(person => person.name === this.state.newName)
+    if (nameExists) {
+      alert('Nimi on jo tallennettu. Kirjoita toinen nimi.');
+      return;
+    }
+
+    const person = {
+      name: this.state.newName
+    }
+
+    const persons = this.state.persons.concat(person)
+
+    this.setState({
+      persons,
+      newName: ''
+    })
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ newName: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addName}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <ul>
+          {this.state.persons.map(person => <li key={person.name}>{person.name}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
